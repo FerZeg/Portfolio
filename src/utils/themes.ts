93ae701e0f1114f35c200bc690e475eb0429cafa
Fernando <ferestga8@gmail.com>
2023-12-31 00:08:23 +0100

    Add themes

---
+++ src/utils/themes.ts
@@ -0,0 +1,20 @@
+interface Theme {
+    name: string;
+    background: string;
+}
+
+
+export const Themes : Theme[] = [
+    {
+        "name": "light",
+        "background": "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)"
+    },
+    {
+        "name": "dark",
+        "background": "linear-gradient(to right, #434343 0%, black 100%);"
+    },
+    {
+        "name": "light2",
+        "background": "linear-gradient(60deg, #abecd6 0%, #fbed96 100%);"
+    }
+]
